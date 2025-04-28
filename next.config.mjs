a30c33ce1ee8317fd862531713e247c2e107f4f2
pratiktariya <pratiktariya000@gmail.com>
2025-04-28 23:25:40 +0530

    Add files via upload
    gaming  webiste code

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['wallpaperaccess.com', 'tse2.mm.bing.net', 'tse4.mm.bing.net'],
+    },
+  };
+  
+  export default nextConfig;
+  
